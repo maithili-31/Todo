8b05465f985a213b57673987c0af482af9801cc9
deepakrvg <guptadeepak0908@gmail.com>
2023-07-20 19:34:23 +0530

    routes created

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const app = express();
 
+const listRoute = require('./routes/list');
+
+const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-    res.send('hello');
-});
+app.use('/list', listRoute);
 
 app.listen(PORT, () => {
     console.log(`server starting at port ${PORT}...`);
